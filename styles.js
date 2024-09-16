c28a2de63f938abd5b63a08ca9ef15ac3e3a7fa6
Fatima Rettea <fatimarettea@Fatimas-MBP.lan>
2024-09-16 08:43:56 -0700

    part 2

--- styles.js
+++ styles.js
@@ -1,7 +1,14 @@
-for (let i = 1; i<=100; i++) {
-    if(i % 3 === 0){ 
-        console.log(i, 'Fizz');
-        }else if(i % 5 == 0 ){
-            console.log(i, 'Buzz')
-        }
+//Part 1: Fizz Ball
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Fizz Buzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
